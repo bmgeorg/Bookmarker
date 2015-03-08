834f0d929c04364b7fb83bde2d75eecf0fc54e49
Brett <bmgeorg@g.clemson.edu>
2015-03-08 13:13:18 -0400

    Add tagging.

--- src/model/Document.java
+++ src/model/Document.java
@@ -1,30 +1,73 @@
 package model;
 
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.TreeMap;
 
 public class Document {
-	private Map<String, Double> words;
+	/*
+	 * p: the cutoff weight for tags
+	 * If a term weight is greater than or equal to p, the term is counted as a tag.
+	 * 
+	 * term weight is the relative frequency of the term in the document
+	 * term weight = term frequency / numTerms * 100
+	 */
+	private double p = .5;
+	private TreeMap<String, Integer> termCounts;
+	private TreeMap<String, Double> tags;
+	private int numTerms;
 	
-	public Document() {
-	}
-
-	public Map<String, Double> getWords() {
-		return words;
+	private void calculateTags() {
+		tags = new TreeMap<String, Double>();
+		Iterator<String> iter = termCounts.keySet().iterator();
+		while(iter.hasNext()) {
+			String term = iter.next();
+			Double weight = weightForTerm(term);
+			if(weight > p)
+				tags.put(term, weight);
+		}
 	}
 	
-	public void setWords(Map<String, Double> words) {
-		this.words = words;
+	/*
+	 * numTerms = count of all terms in document, not just count of distinct terms in document
+	 */
+	public Document(TreeMap<String, Integer> wordCounts, int numTerms) {
+		this.termCounts = wordCounts;
+		this.numTerms = numTerms;
+		
+		calculateTags();
+	}
+		
+	public Double weightForTerm(String term) {
+		if(termCounts.containsKey(term))
+			return (100.0 * termCounts.get(term))/numTerms;
+		else
+			return 0.0;
 	}
 	
-	public void printWords() {
-		Set<String> keys = words.keySet();
-		Iterator<String> iter = keys.iterator();
+	public void printTermCounts() {
+		Iterator<String> iter = termCounts.keySet().iterator();
 		while(iter.hasNext()) {
 			String term = iter.next();
 			System.out.print(term);
-			System.out.println(" " + words.get(term));
+			System.out.println(" " + termCounts.get(term));
+		}
+	}
+	
+	public void printTermWeights() {
+		Iterator<String> iter = termCounts.keySet().iterator();
+		while(iter.hasNext()) {
+			String term = iter.next();
+			System.out.print(term);
+			System.out.println(" " + weightForTerm(term));
+		}
+	}
+	
+	public void printTagWeights() {
+		Iterator<String> iter = tags.keySet().iterator();
+		while(iter.hasNext()) {
+			String tag = iter.next();
+			System.out.print(tag);
+			System.out.println(" " + weightForTerm(tag));
 		}
 	}
 }
--- src/model/Indexer.java
+++ src/model/Indexer.java
@@ -1,50 +1,35 @@
 package model;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.jsoup.Jsoup;
 
 public class Indexer {
 	private org.jsoup.nodes.Document htmlDoc;
-	private Map<String, Double> wordWeights;
+	private TreeMap<String, Integer> wordCounts;
 	private String[] tokens;
 	
 	private void addTerm(String term) {
-		Double count = 1.0;
-		if(wordWeights.containsKey(term)) {
-			count = wordWeights.get(term) + 1;
+		int count = 1;
+		if(wordCounts.containsKey(term)) {
+			count = wordCounts.get(term) + 1;
 		}
-		wordWeights.put(term, count);
+		wordCounts.put(term, count);
 	}
-	
-	private void scaleWeights() {
-		double scale = tokens.length;
-		Set<String> keys = wordWeights.keySet();
-		Iterator<String> iter = keys.iterator();
-		while(iter.hasNext()) {
-			String key = iter.next();
-			wordWeights.put(key, wordWeights.get(key)/scale);
-		}
-	}
+
 	
 	public Document index(String url) throws IOException {
 		htmlDoc = Jsoup.connect(url).get();
 		tokens = new Tokenizer().tokenize(htmlDoc.text());
-		wordWeights = new TreeMap<String, Double>();
+		wordCounts = new TreeMap<String, Integer>();
 		for(String token : tokens) {
 			addTerm(token);
 		}
-		scaleWeights();
-		Document result = new Document();
-		result.setWords(wordWeights);
-		return result;
+		return new Document(wordCounts, tokens.length);
 	}
 	
 	public static void main(String args[]) throws IOException {
-		new Indexer().index("http://lipsum.com").printWords();
+		new Indexer().index("http://martinfowler.com/articles/designDead.html").printTagWeights();
 	}
 }
