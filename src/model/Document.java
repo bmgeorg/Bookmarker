62e399e378d91d85245c8735c2e1d7e9d65d10a4
Brett <bmgeorg@g.clemson.edu>
2015-03-07 21:23:06 -0500

    Add Indexer.

--- src/model/Document.java
+++ src/model/Document.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class Document {
+	private Map<String, Double> words;
 	
+	public Document() {
+	}
+
+	public Map<String, Double> getWords() {
+		return words;
+	}
+	
+	public void setWords(Map<String, Double> words) {
+		this.words = words;
+	}
+	
+	public void printWords() {
+		Set<String> keys = words.keySet();
+		Iterator<String> iter = keys.iterator();
+		while(iter.hasNext()) {
+			String term = iter.next();
+			System.out.print(term);
+			System.out.println(" " + words.get(term));
+		}
+	}
 }
--- src/model/Indexer.java
+++ src/model/Indexer.java
@@ -1,16 +1,50 @@
 package model;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.jsoup.Jsoup;
 
 public class Indexer {
-	public static Document index(String url) throws IOException {
-		System.out.println(Jsoup.connect(url).get().text());
-		return null;
+	private org.jsoup.nodes.Document htmlDoc;
+	private Map<String, Double> wordWeights;
+	private String[] tokens;
+	
+	private void addTerm(String term) {
+		Double count = 1.0;
+		if(wordWeights.containsKey(term)) {
+			count = wordWeights.get(term) + 1;
+		}
+		wordWeights.put(term, count);
 	}
-
-	public static void main(String args[]) throws IOException { 
-		Indexer.index("http://lipsum.org");
+	
+	private void scaleWeights() {
+		double scale = tokens.length;
+		Set<String> keys = wordWeights.keySet();
+		Iterator<String> iter = keys.iterator();
+		while(iter.hasNext()) {
+			String key = iter.next();
+			wordWeights.put(key, wordWeights.get(key)/scale);
+		}
+	}
+	
+	public Document index(String url) throws IOException {
+		htmlDoc = Jsoup.connect(url).get();
+		tokens = new Tokenizer().tokenize(htmlDoc.text());
+		wordWeights = new TreeMap<String, Double>();
+		for(String token : tokens) {
+			addTerm(token);
+		}
+		scaleWeights();
+		Document result = new Document();
+		result.setWords(wordWeights);
+		return result;
+	}
+	
+	public static void main(String args[]) throws IOException {
+		new Indexer().index("http://lipsum.com").printWords();
 	}
 }
