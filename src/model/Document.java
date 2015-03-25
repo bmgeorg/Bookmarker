941590521a78fc5265a8a6e60e026a5f73d0dde5
Brett <bmgeorg@g.clemson.edu>
2015-03-25 09:41:06 -0400

    Clean up style and format of Document. Move weights to [0, 1] instead of [0, 100]

--- src/model/Document.java
+++ src/model/Document.java
@@ -11,17 +11,14 @@ public class Document implements Serializable {
 	private static final long serialVersionUID = -2054790569435840707L;
 	private String url;
 	private HashMap<String, Integer> termCounts;
-	/*
-	 * numTerms = count of all terms in document, not just count of distinct terms in document
-	 */
+	//numTerms = count of all terms in document, not just count of distinct terms in document
 	private int numTerms;
-	/*
-	 * magnitude: the magnitude of the term weight vector in the vector space model
-	 */
+	//magnitude: the magnitude of the term weight vector in the vector space model
 	private double magnitude;
 	
 	public Document(String url) throws IOException {
 		this.url = url;
+		//lie and say we are Firefox -- prevents 403 errors, should probably fix eventually
 		org.jsoup.nodes.Document htmlDoc = Jsoup.connect(url).userAgent("Mozilla").get();
 		String[] tokens = new Tokenizer().tokenize(htmlDoc.text());
 
@@ -39,6 +36,28 @@ public class Document implements Serializable {
 		calculateMagnitude();
 	}
 	
+	public String getURL() {
+		return url;
+	}
+	
+	public Iterator<String> termIterator() {
+		return termCounts.keySet().iterator();
+	}
+		
+	//weight is relative frequency of term among all terms
+	//weight is in [0, 1]
+	public double weightForTerm(String term) {
+		if(termCounts.containsKey(term))
+			return termCounts.get(term)/numTerms;
+		else
+			return 0.0;
+	}
+	
+	public double getMagnitude() {
+		return magnitude;
+	}
+	
+	/* private methods */
 	private void calculateMagnitude() {
 		magnitude = 0;
 		Iterator<String> iter = termIterator();
@@ -48,25 +67,7 @@ public class Document implements Serializable {
 		magnitude = Math.sqrt(magnitude);
 	}
 	
-	public String getURL() {
-		return url;
-	}
-	
-	public Iterator<String> termIterator() {
-		return termCounts.keySet().iterator();
-	}
-		
-	public Double weightForTerm(String term) {
-		if(termCounts.containsKey(term))
-			return (100.0 * termCounts.get(term))/numTerms;
-		else
-			return 0.0;
-	}
-	
-	public Double getMagnitude() {
-		return magnitude;
-	}
-	
+	/* testing */
 	public void printTermCounts() {
 		Iterator<String> iter = termIterator();
 		while(iter.hasNext()) {
@@ -85,6 +86,7 @@ public class Document implements Serializable {
 		}
 	}
 
+	/* overriden methods */
 	@Override
 	public int hashCode() {
 		return url.hashCode();
