e96ee56dcd8f727bceaf69f7a01ee4f6d30c51ef
Brett <bmgeorg@g.clemson.edu>
2015-03-30 12:05:17 -0400

    Add stemming using Tartarus Snowball Porter 2 -- I have some reservations about stemming, but the changes are easily reverted

--- src/model/Tokenizer.java
+++ src/model/Tokenizer.java
@@ -9,11 +9,16 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.tartarus.snowball.EnglishSnowballStemmerFactory;
+import org.tartarus.snowball.util.StemmerException;
+
 //stateless
 public class Tokenizer {
 	//ignored words
 	Set<String> stopwords = new HashSet<String>();
-	
+	EnglishSnowballStemmerFactory stemmer = EnglishSnowballStemmerFactory.getInstance();
+
+
 	public Tokenizer() {
 		//initialize stopwords
 		String fileName = "stopwords.txt";
@@ -27,29 +32,37 @@ public class Tokenizer {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String sanitize(String text) {
 		//remove all non-alphabetic, non-hypen, non + characters
 		text = text.replaceAll("[^a-zA-Z\\-\\+]", " ");
 		text = text.toLowerCase();
 		return text;
 	}
-	
+
 	public String[] tokenize(String text) {
 		text = sanitize(text);
 		ArrayList<String> tokens = new ArrayList<String>();
-		
+
+
 		Pattern pattern = Pattern.compile("\\S+");
 		Matcher matcher = pattern.matcher(text);
 		while(matcher.find()) {
 			String token = matcher.group();
-			if(!stopwords.contains(token))
-				tokens.add(token);
+			if(!stopwords.contains(token)) {
+				//stem token before adding
+				try {
+					tokens.add(stemmer.process(token));
+				} catch(StemmerException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
 		}
-		
+
 		return tokens.toArray(new String[tokens.size()]);
 	}
-	
+
 	public static void main(String args[]) {
 		String tokens[] = new Tokenizer().tokenize("Here is-a sentence 123 from a web blog.Here is another sentence from a web blog!");
 		for(String token : tokens)
